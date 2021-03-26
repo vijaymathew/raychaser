41ee2656c809abc41d8562eca974e44b1813f163
Vijay Mathew <mathew.vijay@gmail.com>
2021-03-26 16:26:22 +0530

    slides

---
+++ slides.js
@@ -0,0 +1,25 @@
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n, slidesClassName="mySlides", dotsClassName="dot") {
+  var i;
+  var slides = document.getElementsByClassName(slidesClassName);
+  var dots = document.getElementsByClassName(dotsClassName);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
